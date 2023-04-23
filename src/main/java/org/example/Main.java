1e6c865ee01320ffc8abbb6c43ea6cc9df674278
MyFirstGirEver <fordevok@gmail.com>
2023-04-23 18:07:46 +0700

    Done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,10 +8,7 @@ import edu.stanford.nlp.simple.Sentence;
 import edu.stanford.nlp.util.CoreMap;
 import edu.stanford.nlp.util.Pair;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.*;
 import java.util.function.Consumer;
 import java.util.regex.MatchResult;
@@ -37,7 +34,7 @@ class TextProcessing{
         }
     }
 
-    static Set<String> lemmas(String doc){
+    static List<String> lemmas(String doc){
         Properties props = new Properties();
         props.put("annotators", "tokenize, ssplit, pos, lemma");
 
@@ -45,12 +42,13 @@ class TextProcessing{
         pipeline = new StanfordCoreNLP(props, false);
         Annotation document = pipeline.process(doc);
 
-        Set<String> lemmas = new TreeSet<>();
+        List<String> lemmas = new ArrayList<>();
         for(CoreMap sentence: document.get(CoreAnnotations.SentencesAnnotation.class))
         {
             for(CoreLabel token: sentence.get(CoreAnnotations.TokensAnnotation.class))
             {
                 String lemma = token.get(CoreAnnotations.LemmaAnnotation.class);
+
                 lemmas.add(lemma.toLowerCase());
             }
         }
@@ -58,11 +56,15 @@ class TextProcessing{
         return lemmas;
     }
 
-    static void removeStopWordsAndWeirdStrings(Set<String> words) throws IOException {
+    static void removeStopWordsAndWeirdStrings(List<String> words) throws IOException {
         Set<String> removed = new TreeSet<>();
 
         for(String word : words){
-            if(Pattern.compile("[!@#$:/.,]").matcher(word).find()){
+//            if(word.equals(":)") || word.equals(":-)") || word.equals(":(") || word.equals(":-(")){
+//                continue;
+//            }
+
+            if(Pattern.compile("^.*[^a-zA-Z].*$").matcher(word).find()){
                 removed.add(word);
             }
         }
@@ -76,42 +78,140 @@ class TextProcessing{
             }
         }
 
-        for(String shoudlRemoveString : removed){
-            words.remove(shoudlRemoveString);
+        for(int i=words.size() - 1;i>=0;i--){
+            if(removed.contains(words.get(i))) {
+                words.remove(i);
+            }
         }
     }
+}
 
-    static HashMap<String, FrequencyTable> establishTable(List<Pair<Set<String>, Integer>> dataset) {
-        HashMap<String, FrequencyTable> table = new HashMap<>();
+class NaiveBayes {
+    private final HashMap<String, TextProcessing.FrequencyTable> table;
+    private final int positiveOccurrences, negativeOccurrences;
+    NaiveBayes(List<Pair<List<String>, Integer>> dataset) {
+        HashMap<String, TextProcessing.FrequencyTable> table = new HashMap<>();
 
-        for(Pair<Set<String>, Integer> pair : dataset){
+        int positiveOccurrences = 0, negativeOccurrences = 0;
+        for(Pair<List<String>, Integer> pair : dataset) {
             for(String word : pair.first) {
-                if(!table.containsKey(word)){
-                    table.put(word, new FrequencyTable(0, 0));
+                if(!table.containsKey(word)) {
+                    table.put(word, new TextProcessing.FrequencyTable(0, 0));
                 }
                 else{
-                    FrequencyTable t = table.get(word);
+                    TextProcessing.FrequencyTable t = table.get(word);
 
                     if(pair.second == 0){
                         t.negativeOccurrence++;
+                        negativeOccurrences++;
                     }
                     else{
                         t.positiveOccurrence++;
+                        positiveOccurrences++;
                     }
                 }
             }
         }
 
-        return table;
+        this.table = table;
+        this.negativeOccurrences = negativeOccurrences;
+        this.positiveOccurrences = positiveOccurrences;
+    }
+
+    public double predict(String tweet) throws IOException {
+        List<String> words = TextProcessing.lemmas(tweet);
+        TextProcessing.removeStopWordsAndWeirdStrings(words);
+
+        Set<String> set = new TreeSet<>(words);
+
+        double pred = 1;
+
+        for(String word : set){
+            pred *= lambda(word);
+        }
+
+        return pred;
+    }
+
+    private double lambda(String word) {
+        TextProcessing.FrequencyTable freq = table.get(word);
+
+        double posPercent, negPercent;
+        if(freq == null){
+            return 1;
+        }
+
+        posPercent = (freq.positiveOccurrence + 1.0f) / (table.size() + positiveOccurrences);
+        negPercent = (freq.negativeOccurrence + 1.0f) / (table.size() + negativeOccurrences);
+
+        return posPercent / negPercent;
     }
 }
 
-class NaiveBayes {
-
-}
-
 public class Main {
+    static List<Pair<List<String>, Integer>> dataset;
     public static void main(String[] args) throws IOException {
+//        dataset = new ArrayList<>();
+//
+//        readData("D:\\Source code\\Java\\data\\train pos.txt", 1);
+//        readData("D:\\Source code\\Java\\data\\train neg.txt", 0);
+//
+//        NaiveBayes model = new NaiveBayes(dataset);
+//
+//        FileInputStream fIn = new FileInputStream("D:\\Source code\\Java\\data\\test pos.txt");
+//        String[] data = new String(fIn.readAllBytes()).split("\3");
+//        fIn.close();
+//
+//        int total = data.length;
+//        int hit = 0;
+//
+//        for(String tweet : data) {
+//            double pred = model.predict(tweet);
+//
+//            boolean isPositive = false;
+//            if(pred >= 1.0f){
+//                isPositive = true;
+//            }
+//
+//            if(isPositive){
+//                hit++;
+//            }
+//        }
+//
+//        fIn = new FileInputStream("D:\\Source code\\Java\\data\\test neg.txt");
+//        data = new String(fIn.readAllBytes()).split("\3");
+//        fIn.close();
+//
+//        total += data.length;
+//
+//        for(String tweet : data) {
+//            double pred = model.predict(tweet);
+//
+//            boolean isPositive = false;
+//            if(pred >= 1.0f){
+//                isPositive = true;
+//            }
+//
+//            if(!isPositive){
+//                hit++;
+//            }
+//        }
+//
+//        System.out.println(hit / (float)total * 100 + "%");
 
+    }
+
+    static void readData(String fileName, int label) throws IOException {
+        FileInputStream fIn = new FileInputStream(fileName);
+        String[] data = new String(fIn.readAllBytes()).split("\3");
+
+        for(String tweet : data){
+            List<String> words = TextProcessing.lemmas(tweet);
+            TextProcessing.removeStopWordsAndWeirdStrings(words);
+
+            dataset.add(new Pair<>(words, label));
+        }
+
+        fIn.close();
     }
 }
